2f92c30863939854c2634368026731d9ad788668
Alisher Askar <alisher.askar.work@gmail.com>
2026-07-29 16:12:22 +0500

    chore: configure eslint, prettier, and typescript

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+
+  env: {
+    browser: true,
+    es2023: true,
+    node: true,
+  },
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    project: ['./tsconfig.app.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+
+  plugins: ['@typescript-eslint'],
+
+  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'prettier'],
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+
+    'import/prefer-default-export': 'off',
+
+    'react/require-default-props': 'off',
+
+    '@typescript-eslint/consistent-type-imports': 'error',
+
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
-export const App = () => {
-	return <div>App</div>;
-};
+export function App() {
+  return <div>App</div>;
+}
